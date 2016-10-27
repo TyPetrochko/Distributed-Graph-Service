35e8b3abf7afdae67d2bad1147d491f0aaa6e5f0
Robert Tung <rrt23@gator.zoo.cs.yale.edu>
2016-10-26 22:15:22 -0400

    woops remove extraneous test lines

--- main.cpp
+++ main.cpp
@@ -15,8 +15,6 @@
 
 using namespace std;
 
-#define TESTCHECK true
-
 // default error message
 string err_msg = string("HTTP/1.1 400 Bad Request\r\n")
   + "Content-Length: 0\r\n"
@@ -28,10 +26,6 @@ bool format;
 string dev_file;
 
 void process_args(int argc, char **argv) {
-	if (TESTCHECK && argc == 2 && argv[1] == "-c") {
-		checkpoint();
-		return;
-	}
 	if (argc < 3 || argc > 4){
 		cerr << "Usage: ./cs426_graph_server [-f] <port> <devfile>" << endl;
 		exit(1);
